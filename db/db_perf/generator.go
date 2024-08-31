57fe59b1e3e1bfbe9d62e83c82bd9ebc0bbab0c5
ihewe <ihewe001@gmail.com>
2024-09-01 00:01:56 +0800

    range options

--- db/db_perf/generator.go
+++ db/db_perf/generator.go
@@ -3,26 +3,77 @@ package db_perf
 import (
 	"fmt"
 	"gorm.io/gorm"
+	"math"
 	"math/rand"
 	"reflect"
+	"strconv"
 )
 
 const (
-	maxRandIntUpperLimit = 1000_000
-	maxStringLength      = 50
+	maxStringLength = 20
 )
 
+var (
+	// I think even table type is bigint, although the max value is 2^32-1 is enough
+	defaultValueRanges = map[reflect.Kind]valueRange{
+		reflect.Int:     {lower: 0, upper: math.MaxInt32},
+		reflect.Int8:    {lower: 0, upper: math.MaxInt8},
+		reflect.Int16:   {lower: 0, upper: math.MaxInt16},
+		reflect.Int32:   {lower: 0, upper: math.MaxInt32},
+		reflect.Int64:   {lower: 0, upper: math.MaxInt32},
+		reflect.Uint:    {lower: 0, upper: math.MaxUint32},
+		reflect.Uint8:   {lower: 0, upper: math.MaxUint8},
+		reflect.Uint16:  {lower: 0, upper: math.MaxUint16},
+		reflect.Uint32:  {lower: 0, upper: math.MaxUint32},
+		reflect.Uint64:  {lower: 0, upper: math.MaxUint32},
+		reflect.Float32: {lowerFloat: 0.0, upperFloat: math.MaxFloat32},
+		reflect.Float64: {lowerFloat: 0.0, upperFloat: math.MaxFloat32},
+		reflect.String:  {strLen: maxStringLength},
+	}
+)
+
+type valueGenerator func(model any)
+type valueRange struct {
+	// int options
+	lower, upper int
+
+	// float options
+	lowerFloat, upperFloat float64
+
+	// random string options
+	strOptions []string
+	strLen     int
+}
+
 type MassDataGenerator struct {
-	tx *gorm.DB
+	tx     *gorm.DB
+	ranges map[reflect.Kind]valueRange
 }
 
-func NewMassDataGenerator(db *gorm.DB) *MassDataGenerator {
-	return &MassDataGenerator{tx: db}
+type OptionFunc func(*MassDataGenerator)
+
+func WithRanges(ranges map[reflect.Kind]valueRange) OptionFunc {
+	return func(g *MassDataGenerator) {
+		g.ranges = ranges
+	}
 }
 
-func (g *MassDataGenerator) GenerateDataWithoutDataSet(model any, count int) error {
+func NewMassDataGenerator(db *gorm.DB, opts ...OptionFunc) *MassDataGenerator {
+	g := &MassDataGenerator{
+		tx:     db,
+		ranges: defaultValueRanges,
+	}
+
+	for _, opt := range opts {
+		opt(g)
+	}
+
+	return g
+}
+
+func (g *MassDataGenerator) InsertMassData(model any, count int) error {
 	for i := 0; i < count; i++ {
-		generateValuesForModel(model)
+		g.generateValuesForModel(model)
 		if err := g.tx.Create(model).Error; err != nil {
 			return err
 		}
@@ -30,8 +81,12 @@ func (g *MassDataGenerator) GenerateDataWithoutDataSet(model any, count int) err
 	return nil
 }
 
+func (g *MassDataGenerator) UpdateRange(kind reflect.Kind, r valueRange) {
+	g.ranges[kind] = r
+}
+
 // generateValuesForModel generates values for model in reflection way
-func generateValuesForModel(model any) {
+func (g *MassDataGenerator) generateValuesForModel(model any) {
 	v := reflect.ValueOf(model)
 
 	// 确保传入的是一个指针并且是指向结构体的
@@ -58,13 +113,13 @@ func generateValuesForModel(model any) {
 		if field.CanSet() { // 检查字段是否可以被设置
 			switch field.Kind() {
 			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-				field.SetInt(int64(rand.Intn(maxRandIntUpperLimit))) // 设置一个随机整数
+				g.setInt(field.Kind(), field) // 设置一个随机整数
 			case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-				field.SetUint(uint64(rand.Intn(maxRandIntUpperLimit))) // 设置一个随机无符号整数
+				g.setInt(field.Kind(), field) // 设置一个随机整数
 			case reflect.Float32, reflect.Float64:
-				field.SetFloat(rand.Float64() * maxRandIntUpperLimit) // 设置一个随机浮点数
+				g.setFloat(field.Kind(), field) // 设置一个随机浮点数
 			case reflect.String:
-				field.SetString(generateRandomString(rand.Intn(maxStringLength) + 1)) // 设置一个随机字符串
+				g.setString(field.Kind(), field, fieldType) // 设置一个随机字符串
 			case reflect.Bool:
 				field.SetBool(rand.Intn(2) == 1) // 随机布尔值
 			}
@@ -72,6 +127,43 @@ func generateValuesForModel(model any) {
 			fmt.Printf("字段 %s 无法设置\n", fieldType.Name)
 		}
 	}
+}
+
+// setInt get value by int kind ranges
+func (g *MassDataGenerator) setInt(kind reflect.Kind, field reflect.Value) {
+	val := int64(rand.Intn(g.ranges[kind].upper-g.ranges[kind].lower) + g.ranges[kind].lower)
+	field.SetInt(val)
+}
+
+// setFloat get value by float kind ranges
+func (g *MassDataGenerator) setFloat(kind reflect.Kind, field reflect.Value) {
+	val := rand.Float64()*(g.ranges[kind].upperFloat-g.ranges[kind].lowerFloat) + g.ranges[kind].lowerFloat
+	field.SetFloat(val)
+}
+
+// setString get value by string kind ranges
+func (g *MassDataGenerator) setString(kind reflect.Kind, field reflect.Value, fieldType reflect.StructField) {
+	// if has options, select one from options
+	if len(g.ranges[kind].strOptions) > 0 {
+		field.SetString(randSelectOneFromOptions(g.ranges[kind].strOptions))
+		return
+	}
+
+	// get gorm tag size
+	size, _ := strconv.Atoi(fieldType.Tag.Get("size"))
+
+	// get min str len if size > 0
+	strLen := g.ranges[kind].strLen
+	if size > 0 && size < strLen {
+		strLen = size
+	}
+
+	// set random string
+	field.SetString(generateRandomString(rand.Intn(strLen) + 1))
+}
+
+func randSelectOneFromOptions(options []string) string {
+	return options[rand.Intn(len(options))]
 }
 
 // generateRandomString 生成一个随机字符串
--- db/db_perf/generator_test.go
+++ db/db_perf/generator_test.go
@@ -2,6 +2,7 @@ package db_perf
 
 import (
 	"gorm.io/gorm"
+	"reflect"
 	"testing"
 )
 import "gorm.io/driver/mysql"
@@ -19,6 +20,7 @@ type PerfTest struct {
 	ID         int     `gorm:"primaryKey"`
 	Name       string  `gorm:"size:255"`
 	FloatPrice float64 `gorm:"type:float"`
+	Price      float64 `gorm:"type:decimal(8,2)"`
 }
 
 func (p *PerfTest) TableName() string {
@@ -41,7 +43,7 @@ func TestMassDataGenerator_GenerateDataWithoutDataSet(t *testing.T) {
 
 	// insert data
 	g := NewMassDataGenerator(tx)
-	err = g.GenerateDataWithoutDataSet(&PerfTest{}, count)
+	err = g.InsertMassData(&PerfTest{}, count)
 	if err != nil {
 		t.Error(err)
 		return
@@ -58,4 +60,62 @@ func TestMassDataGenerator_GenerateDataWithoutDataSet(t *testing.T) {
 		t.Errorf("expect %d, got %d", startCount+int64(count), endCount)
 		return
 	}
+}
+
+func TestMassDataGenerator_GenerateDataSetRange(t *testing.T) {
+	db := initDB()
+	tx := db.Begin()
+	defer tx.Rollback()
+
+	count := 10
+	// get data len before insert
+	var startCount int64
+	err := tx.Model(&PerfTest{}).Count(&startCount).Error
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	// insert data
+	strOptions := []string{"你好", "hello", "こんにちは", "안녕", "Bonjour"}
+	g := NewMassDataGenerator(tx, WithRanges(map[reflect.Kind]valueRange{
+		reflect.String: {strOptions: strOptions},
+	}))
+	err = g.InsertMassData(&PerfTest{}, count)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	// get data len after insert
+	var endCount int64
+	err = tx.Model(&PerfTest{}).Count(&endCount).Error
+	if err != nil {
+		t.Error(err)
+	}
+
+	if endCount != startCount+int64(count) {
+		t.Errorf("expect %d, got %d", startCount+int64(count), endCount)
+		return
+	}
+
+	// get last data check if name is in strOptions
+	var lastData PerfTest
+	err = tx.Last(&lastData).Error
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	strOptMap := make(map[string]bool)
+	for _, opt := range strOptions {
+		strOptMap[opt] = true
+	}
+
+	if !strOptMap[lastData.Name] {
+		t.Errorf("expect %v, got %v", strOptions, lastData.Name)
+		return
+	}
+
+	t.Log("success")
 }
