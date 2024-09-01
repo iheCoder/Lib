18335fee3083f815e75bdd447375e7865f75eebf
ihewe <ihewe001@gmail.com>
2024-09-01 11:05:52 +0800

    struct filler

--- db/db_perf/generator.go
+++ db/db_perf/generator.go
@@ -7,10 +7,13 @@ import (
 	"math/rand"
 	"reflect"
 	"strconv"
+	"strings"
 )
 
 const (
 	maxStringLength = 20
+	minStringLength = 1
+	genTagName      = "gen"
 )
 
 var (
@@ -28,11 +31,43 @@ var (
 		reflect.Uint64:  {lower: 0, upper: math.MaxUint32},
 		reflect.Float32: {lowerFloat: 0.0, upperFloat: math.MaxFloat32},
 		reflect.Float64: {lowerFloat: 0.0, upperFloat: math.MaxFloat32},
-		reflect.String:  {strLen: maxStringLength},
+		reflect.String:  {strLenMax: maxStringLength},
 	}
+
+	errNotStructPtr = fmt.Errorf("传入的必须是结构体指针")
 )
 
-type valueGenerator func(model any)
+type gormTag struct {
+	PrimaryKey bool
+	Size       int
+	Type       string
+}
+
+func parseGormTag(tag string) *gormTag {
+	if len(tag) == 0 {
+		return nil
+	}
+
+	t := gormTag{}
+	tags := strings.Split(tag, ";")
+	for _, tag := range tags {
+		parts := strings.Split(tag, ":")
+		if len(parts) != 2 {
+			continue
+		}
+		switch parts[0] {
+		case "primaryKey":
+			t.PrimaryKey = true
+		case "size":
+			t.Size, _ = strconv.Atoi(parts[1])
+		case "type":
+			t.Type = parts[1]
+		}
+	}
+	return &t
+}
+
+type valueGenerator func() any
 type valueRange struct {
 	// int options
 	lower, upper int
@@ -41,13 +76,14 @@ type valueRange struct {
 	lowerFloat, upperFloat float64
 
 	// random string options
-	strOptions []string
-	strLen     int
+	strOptions           []string
+	strLenMax, strLenMin int
 }
 
 type MassDataGenerator struct {
-	tx     *gorm.DB
-	ranges map[reflect.Kind]valueRange
+	tx      *gorm.DB
+	ranges  map[reflect.Kind]valueRange
+	valGens map[string]valueGenerator
 }
 
 type OptionFunc func(*MassDataGenerator)
@@ -58,10 +94,17 @@ func WithRanges(ranges map[reflect.Kind]valueRange) OptionFunc {
 	}
 }
 
+func WithValueGenerators(valGens map[string]valueGenerator) OptionFunc {
+	return func(g *MassDataGenerator) {
+		g.valGens = valGens
+	}
+}
+
 func NewMassDataGenerator(db *gorm.DB, opts ...OptionFunc) *MassDataGenerator {
 	g := &MassDataGenerator{
-		tx:     db,
-		ranges: defaultValueRanges,
+		tx:      db,
+		ranges:  defaultValueRanges,
+		valGens: make(map[string]valueGenerator),
 	}
 
 	for _, opt := range opts {
@@ -86,13 +129,12 @@ func (g *MassDataGenerator) UpdateRange(kind reflect.Kind, r valueRange) {
 }
 
 // generateValuesForModel generates values for model in reflection way
-func (g *MassDataGenerator) generateValuesForModel(model any) {
+func (g *MassDataGenerator) generateValuesForModel(model any) error {
 	v := reflect.ValueOf(model)
 
 	// 确保传入的是一个指针并且是指向结构体的
 	if v.Kind() != reflect.Ptr || v.Elem().Kind() != reflect.Struct {
-		fmt.Println("传入的必须是结构体指针")
-		return
+		return errNotStructPtr
 	}
 
 	v = v.Elem()  // 获取结构体的Value
@@ -104,9 +146,21 @@ func (g *MassDataGenerator) generateValuesForModel(model any) {
 		fieldType := t.Field(i)
 
 		// 检查是否为主键，主键跳过，并设置零值
-		if fieldType.Tag.Get("gorm") == "primaryKey" {
-			field.Set(reflect.Zero(field.Type()))
+		gt := parseGormTag(fieldType.Tag.Get("gorm"))
+		if gt != nil && gt.PrimaryKey {
 			continue
+		}
+
+		// 检查是否有gen tag，取出key，并使用对应的生成器生成值
+		genTag := fieldType.Tag.Get(genTagName)
+		if genTag != "" {
+			key := searchKey(genTag)
+			if len(key) > 0 {
+				if err := g.setValueFromGenerator(field, key); err != nil {
+					return err
+				}
+				continue
+			}
 		}
 
 		// 根据字段类型设置随机值
@@ -117,16 +171,39 @@ func (g *MassDataGenerator) generateValuesForModel(model any) {
 			case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
 				g.setInt(field.Kind(), field) // 设置一个随机整数
 			case reflect.Float32, reflect.Float64:
-				g.setFloat(field.Kind(), field) // 设置一个随机浮点数
+				g.setFloat(field.Kind(), field, gt) // 设置一个随机浮点数
 			case reflect.String:
-				g.setString(field.Kind(), field, fieldType) // 设置一个随机字符串
+				g.setString(field.Kind(), field, gt) // 设置一个随机字符串
 			case reflect.Bool:
 				field.SetBool(rand.Intn(2) == 1) // 随机布尔值
 			}
 		} else {
-			fmt.Printf("字段 %s 无法设置\n", fieldType.Name)
+			return fmt.Errorf("field %s can't be set", fieldType.Name)
 		}
 	}
+	return nil
+}
+
+func searchKey(tag string) string {
+	// split tag by ','
+	tags := strings.Split(tag, ",")
+	for _, t := range tags {
+		// split tag by ':'
+		parts := strings.Split(t, ":")
+		if len(parts) == 2 && parts[0] == "key" {
+			return parts[1]
+		}
+	}
+
+	return ""
+}
+
+func (g *MassDataGenerator) setValueFromGenerator(field reflect.Value, key string) error {
+	if gen, ok := g.valGens[key]; ok {
+		field.Set(reflect.ValueOf(gen()))
+		return nil
+	}
+	return fmt.Errorf("no generator found for key %s", key)
 }
 
 // setInt get value by int kind ranges
@@ -136,30 +213,47 @@ func (g *MassDataGenerator) setInt(kind reflect.Kind, field reflect.Value) {
 }
 
 // setFloat get value by float kind ranges
-func (g *MassDataGenerator) setFloat(kind reflect.Kind, field reflect.Value) {
-	val := rand.Float64()*(g.ranges[kind].upperFloat-g.ranges[kind].lowerFloat) + g.ranges[kind].lowerFloat
+func (g *MassDataGenerator) setFloat(kind reflect.Kind, field reflect.Value, tag *gormTag) {
+	upperFloat := g.ranges[kind].upperFloat
+	lowerFloat := g.ranges[kind].lowerFloat
+	// handle decimal special
+	if tag != nil && strings.Contains(tag.Type, "decimal") {
+		// remove "decimal(" and ")", get prec and scale
+		tp := tag.Type
+		tp = tp[8 : len(tp)-1]
+		parts := strings.Split(tp, ",")
+		scale, _ := strconv.Atoi(parts[0])
+		prec, _ := strconv.Atoi(parts[1])
+		if scale > 0 {
+			upperFloat = math.Pow10(scale)
+			val := rand.Float64()*(upperFloat-lowerFloat) + lowerFloat
+			val = float64(int(val)) / math.Pow10(prec)
+			field.SetFloat(val)
+			return
+		}
+	}
+
+	val := rand.Float64()*(upperFloat-lowerFloat) + lowerFloat
 	field.SetFloat(val)
 }
 
 // setString get value by string kind ranges
-func (g *MassDataGenerator) setString(kind reflect.Kind, field reflect.Value, fieldType reflect.StructField) {
+func (g *MassDataGenerator) setString(kind reflect.Kind, field reflect.Value, tag *gormTag) {
 	// if has options, select one from options
 	if len(g.ranges[kind].strOptions) > 0 {
 		field.SetString(randSelectOneFromOptions(g.ranges[kind].strOptions))
 		return
 	}
 
-	// get gorm tag size
-	size, _ := strconv.Atoi(fieldType.Tag.Get("size"))
-
 	// get min str len if size > 0
-	strLen := g.ranges[kind].strLen
-	if size > 0 && size < strLen {
-		strLen = size
+	strLenMin := g.ranges[kind].strLenMin
+	strLenMax := g.ranges[kind].strLenMax
+	if tag != nil && tag.Size > 0 && tag.Size < strLenMax {
+		strLenMax = tag.Size
 	}
 
 	// set random string
-	field.SetString(generateRandomString(rand.Intn(strLen) + 1))
+	field.SetString(generateRandomString(rand.Intn(strLenMax-strLenMin) + strLenMin))
 }
 
 func randSelectOneFromOptions(options []string) string {
--- db/db_perf/generator_test.go
+++ db/db_perf/generator_test.go
@@ -1,7 +1,9 @@
 package db_perf
 
 import (
+	"encoding/json"
 	"gorm.io/gorm"
+	"math/rand"
 	"reflect"
 	"testing"
 )
@@ -21,6 +23,13 @@ type PerfTest struct {
 	Name       string  `gorm:"size:255"`
 	FloatPrice float64 `gorm:"type:float"`
 	Price      float64 `gorm:"type:decimal(8,2)"`
+	Info       string  `gorm:"type:json" gen:"key:extra"`
+}
+
+type PerfTestExtra struct {
+	Score  int     `json:"score"`
+	Degree string  `json:"degree"`
+	Weight float64 `json:"weight"`
 }
 
 func (p *PerfTest) TableName() string {
@@ -114,6 +123,86 @@ func TestMassDataGenerator_GenerateDataSetRange(t *testing.T) {
 
 	if !strOptMap[lastData.Name] {
 		t.Errorf("expect %v, got %v", strOptions, lastData.Name)
+		return
+	}
+
+	t.Log("success")
+}
+
+func TestMassDataGenerator_GenerateDataSetValueGenerator(t *testing.T) {
+	db := initDB()
+	tx := db.Begin()
+	defer tx.Rollback()
+
+	count := 10
+	// get data len before insert
+	var startCount int64
+	err := tx.Model(&PerfTest{}).Count(&startCount).Error
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	// insert data
+	valGens := map[string]valueGenerator{
+		"extra": func() any {
+			score := rand.Intn(700) + 1
+			degree := []string{"小学", "初中", "高中", "本科", "硕士", "博士"}[rand.Intn(6)]
+			weight := float64(rand.Intn(150)+50) + rand.Float64()
+			extra := PerfTestExtra{
+				Score:  score,
+				Degree: degree,
+				Weight: weight,
+			}
+
+			eb, _ := json.Marshal(extra)
+			return string(eb)
+		},
+	}
+	g := NewMassDataGenerator(tx, WithValueGenerators(valGens))
+	err = g.InsertMassData(&PerfTest{}, count)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	// get data len after insert
+	var endCount int64
+	err = tx.Model(&PerfTest{}).Count(&endCount).Error
+	if err != nil {
+		t.Error(err)
+	}
+
+	if endCount != startCount+int64(count) {
+		t.Errorf("expect %d, got %d", startCount+int64(count), endCount)
+		return
+	}
+
+	// get last data check if name is in strOptions
+	var lastData PerfTest
+	err = tx.Last(&lastData).Error
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	var extra PerfTestExtra
+	err = json.Unmarshal([]byte(lastData.Info), &extra)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if extra.Score < 1 || extra.Score > 700 {
+		t.Errorf("expect 1-700, got %d", extra.Score)
+		return
+	}
+	if extra.Degree == "" {
+		t.Errorf("expect not empty, got %s", extra.Degree)
+		return
+	}
+	if extra.Weight < 50 || extra.Weight > 200 {
+		t.Errorf("expect 50-200, got %f", extra.Weight)
 		return
 	}
 
