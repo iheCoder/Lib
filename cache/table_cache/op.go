7b73a08c7f77496ebcb190d4ecef47555cb26e04
ihewe <ihewe001@gmail.com>
2024-10-16 22:33:52 +0800

    op has version

--- cache/table_cache/op.go
+++ cache/table_cache/op.go
@@ -4,12 +4,17 @@ type TableCacheOp struct {
 	IDxData map[string]any
 	data    any
 	config  *TablePullConfig
+	version int64
 }
 
 func (i *TableCacheOp) GetModelByID(id string) any {
 	return i.IDxData[id]
 }
 
-func (i *TableCacheOp) GetData() any {
-	return i.data
+func (i *TableCacheOp) GetData() (any, int64) {
+	return i.data, i.version
+}
+
+func (i *TableCacheOp) CheckDataUpdated(version int64) bool {
+	return i.version != version
 }
