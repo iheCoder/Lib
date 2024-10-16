bc0317fcebb1619281cfcb3bd86fa55a182b9f61
heliwei <heliwei@tal.com>
2024-10-16 18:00:15 +0800

    startUpdateOpsData

---
+++ cache/table_cache/updater.go
@@ -0,0 +1,28 @@
+package table_cache
+
+import (
+	"github.com/RussellLuo/timingwheel"
+	"time"
+)
+
+type tableOpScheduler struct {
+	baseInterval time.Duration
+}
+
+func (s *tableOpScheduler) Next(prev time.Time) time.Time {
+	return prev.Add(s.baseInterval)
+}
+
+func (mgr *TableCacheMgr) startUpdateOpsData() {
+	// start a timing wheel
+	tw := timingwheel.NewTimingWheel(time.Second, 60)
+	tw.Start()
+	defer tw.Stop()
+
+	// add update ops to timing wheel
+	for key, op := range mgr.ops {
+		tw.ScheduleFunc(&tableOpScheduler{op.config.UpdateInterval}, func() {
+			mgr.pullTableData(*op.config, key)
+		})
+	}
+}
