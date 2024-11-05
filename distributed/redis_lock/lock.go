69b4b263527af9aee60b95a234db8341c89d31c8
iheCoder <heliwei@tal.com>
2024-11-05 17:56:21 +0800

    auto-renew after lock success

--- distributed/redis_lock/lock.go
+++ distributed/redis_lock/lock.go
@@ -24,9 +24,29 @@ type RedisLock struct {
 }
 
 func (l *RedisLock) Lock() error {
-	return l.client.SetNX(l.ctx, l.key, l.value, l.ttl).Err()
+	err := l.client.SetNX(l.ctx, l.key, l.value, l.ttl).Err()
+	if err != nil {
+		return err
+	}
+
+	go l.renew()
+	return nil
 }
 
 func (l *RedisLock) Unlock() error {
 	return l.client.Eval(l.ctx, delLuaScript, []string{l.key}, l.value).Err()
+}
+
+func (l *RedisLock) renew() {
+	ticker := time.NewTicker(l.ttl / 2)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-l.ctx.Done():
+			return
+		case <-ticker.C:
+			l.client.Expire(l.ctx, l.key, l.ttl)
+		}
+	}
 }
